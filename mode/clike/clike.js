7c0105a6b4464bab4fd5c64204fc2d1e04d12ba4
Maura Warner <mwarner@gsgenetics.com>
2017-04-20 16:12:17 -0400

    separating files out in the hopes of making a small npm package

--- mode/clike/clike.js
+++ mode/clike/clike.js
@@ -447,41 +447,6 @@ CodeMirror.defineMode("clike", function(config, parserConfig) {
     modeProps: {fold: ["brace", "import"]}
   });
 
-  def("text/x-apex", {
-    name: "clike",
-    keywords: words( // From Java
-                    "abstract assert break catch class const continue default " +
-                    "do else enum extends final finally float for if implements " +
-                    "instanceof interface new package private protected public " +
-                    "return static super this throw try while " +
-                    // Apex only
-                    "global continue decimal default future global tolabel webservice virtual " +
-                    // SOQL
-                    "and AND as AS asc ASC bulk BULK by BY commit COMMIT delete DELETE " +
-                    "from FROM insert INSERT last_90_days last_month last_n_days last_week " +
-                    "LAST_90_DAYS LAST_MONTH LAST_N_DAYS LAST_WEEK like LIKE" +
-                    "limit LIMIT merge MERGE not NOT on ON or OR rollback ROLLBACK " +
-                    "select SELECT set SET sort SORT this_week THIS_WEEK " +
-                    "today TODAY tomorrow TOMORROW undelete UNDELETE update UPDATE " +
-                    "upsert UPSERT where WHERE yesterday YESTERDAY using USING " +
-                    "convertcurrency CONVERTCURRENCY after AFTER before BEFORE count COUNT " +
-                    "excludes EXCLUDES first FIRST includes INCLUDES last LAST order ORDER " +
-                    "sharing SHARING with WITH test"),
-    types: words("byte short int long float double boolean void Boolean Byte Character Double Float " +
-                 "Integer Long Number Object Short String Void blob Blob RestContext System system"),
-    blockKeywords: words("catch class do else finally for if switch try while"),
-    defKeywords: words("class interface package enum @interface"),
-    typeFirstDefinitions: true,
-    atoms: words("true false null nulls TRUE FALSE NULL NULLS"),
-    hooks: {
-      "@": function(stream) {
-        stream.eatWhile(/[\w\$_]/);
-        return "meta";
-      }
-    },
-    modeProps: {fold: ["brace"]}
-  });
-
   def("text/x-csharp", {
     name: "clike",
     keywords: words("abstract as async await base break case catch checked class const continue" +
